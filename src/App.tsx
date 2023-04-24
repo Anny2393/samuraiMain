5c92ddafde53cff82bb4616485b4b5e8a6b8b51a
Anna Narushevich <annyit23@gmail.com>
2023-04-24 23:02:23 +0300

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Components/Technologies";
-import Header from "./Components/Header";
+
 
 const App = () => {
     return (
-    <div>
-        <Header />
-        <Technologies />
-      </div>);
+        <div className='app-wrapper'>
+            <header className="header">
+                <img
+                    src='https://www.pngmart.com/files/13/Akatsuki-Logo-PNG-Pic.png'>
+                </img>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src='https://png.pngtree.com/thumb_back/fh260/back_our/20190617/ourmid/pngtree-chinese-style-traditional-beijing-opera-mask-background-material-image_126730.jpg'></img>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                </div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
